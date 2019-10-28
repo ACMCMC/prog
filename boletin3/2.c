65a10120ed7f463dbcbbceeeaa769bd475276ec2
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:23:05 +0100

    Update 2.c

--- boletin3/2.c
+++ boletin3/2.c
@@ -13,28 +13,28 @@ Este programa devolve un vector de caracteres (que introduce o usuario), pero ó
 /*Esta función dalle a volta ó vector de chars*/
 
 char* backwards(char palabra[], int lonxitude, char* saveto) {
-int index;
-for(int i = 0; i < (lonxitude + 1); i++) {
-index = lonxitude-i-1;
-saveto[i] = palabra[index];
-}
-saveto[lonxitude] = '\0';
-return(saveto);
+    int index;
+    for (int i = 0; i < (lonxitude + 1); i++) {
+        index = lonxitude - i - 1;
+        saveto[i] = palabra[index];
+    }
+    saveto[lonxitude] = '\0';
+    return (saveto);
 }
 
 int main() {
-char palabra[100]; /*declaramos variables*/
-int num_caracteres;
-/*preguntámoslle unha palabra ao usuario*/
+    char palabra[100]; /*declaramos variables*/
+    int num_caracteres;
+    /*preguntámoslle unha palabra ao usuario*/
 
-printf("Escriba unha palabra:\n");
-scanf("%s",palabra);
+    printf("Escriba unha palabra:\n");
+    scanf("%s", palabra);
 
-num_caracteres = strlen(palabra);
-char back[num_caracteres];
+    num_caracteres = strlen(palabra);
+    char back[num_caracteres];
 
-/*Dámoslle a volta á palabra*/
-backwards(palabra,num_caracteres,back);
-printf("%s",back);
-return(EXIT_SUCCESS);
+    /*Dámoslle a volta á palabra*/
+    backwards(palabra, num_caracteres, back);
+    printf("%s", back);
+    return (EXIT_SUCCESS);
 }
