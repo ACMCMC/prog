a23e10f24ac0ec041fc8c7ddc55f25034f8374d4
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:27:53 +0100

    Create 5.c

---
+++ boletin3/5.c
@@ -0,0 +1,20 @@
+/*ENUNCIADO
+Escribir un programa cunha función que determine a media dos valores dunha secuencia de
+enteiros almacenada nun arquivo. Lede os datos do arquivo e ideos gardando nun vector. Facede a
+lectura ata chegar ao final o arquivo (nese momento feof(ptf)!=0). No arquivo cabeceira stdlib.h
+está definida a función de formato int feof(FILE *ptf), que ten coma argumento o punteiro a arquivo
+e devolve un valor distinto de cero cando chega ao final do arquivo e cero noutro caso.*/
+
+/*Autor: Aldán Creo Mariño
+Data: 28 de outubro de 2019
+Versión: 1
+Este programa determina a media dos valores dunha secuencia de enteiros almacenados nun arquivo*/
+
+#define EXIT_SUCCESS 0
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  return(EXIT_SUCCESS);
+}
