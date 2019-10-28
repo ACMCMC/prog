4400fddc2d395fb2535d1471f0d2de1d815d3526
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:30:37 +0100

    Update 7.c

--- boletin3/7.c
+++ boletin3/7.c
@@ -4,3 +4,17 @@ Escribir un programa que lea un texto almacenado nun arquivo, encriptado de acor
 descrita antes, descifre tal texto e o presente descifrado por pantalla. Empregar unha función para o
 desencriptado. Para desencriptar hai que sumarlle 128 á resta de códigos e despois cacular o
 módulo 128*/
+
+/*Autor: Aldán Creo Mariño
+Data: 28 de outubro de 2019
+Versión: 1
+Este programa traballa con datos encriptados*/
+
+#define EXIT_SUCCESS 0
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  return(EXIT_SUCCESS);
+}
