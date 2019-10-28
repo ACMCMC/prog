d4b14a69cf7b1ba5e7c70ed7231f82d52d3e19d3
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:24:59 +0100

    Update 1b.c

--- boletin3/1b.c
+++ boletin3/1b.c
@@ -1,24 +1,23 @@
 /*Autor: Aldán Creo Mariño
 Data: 16 de setembro de 2019
 Versión: 1
-
 Este programa devolve o máximo de dous enteiros pasados como argumentos a unha función*/
 
-int* encontmax(int a, int b) {
-int* max;
-if (a >= b) {
-max = &a;
-} else {
-max = &b;
-}
-return(max);
+int * encontmax(int a, int b) {
+  int * max;
+  if (a >= b) {
+    max = & a;
+  } else {
+    max = & b;
+  }
+  return (max);
 }
 
-main() { 
-int a,b;
-int resultado;
-printf("Dame dous valores enteiros: ");
-scanf("%d %d",&a,&b);
-resultado = *encontmax(a, b); 
-printf("Máximo=%d\n",resultado); 
+main() {
+  int a, b;
+  int resultado;
+  printf("Dame dous valores enteiros: ");
+  scanf("%d %d", & a, & b);
+  resultado = * encontmax(a, b);
+  printf("Máximo=%d\n", resultado);
 }
