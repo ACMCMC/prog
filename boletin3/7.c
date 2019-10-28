e379f9d34cfacfc1772047af9a36cf761b52a7cb
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:30:13 +0100

    Create 7.c

---
+++ boletin3/7.c
@@ -0,0 +1,6 @@
+/*ENUNCIADO
+
+Escribir un programa que lea un texto almacenado nun arquivo, encriptado de acordo ca técnica
+descrita antes, descifre tal texto e o presente descifrado por pantalla. Empregar unha función para o
+desencriptado. Para desencriptar hai que sumarlle 128 á resta de códigos e despois cacular o
+módulo 128*/
