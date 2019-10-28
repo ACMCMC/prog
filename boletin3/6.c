7425ea9e2c88dadf283524899f0236ac2522c895
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2019-10-28 17:28:18 +0100

    Create 6.c

---
+++ boletin3/6.c
@@ -0,0 +1,10 @@
+/*Escribir un programa que lea un arquivo de texto e que garde noutro arquivo ese texto encriptado
+da seguinte forma: o primeiro carácter imprímeo tal como foi lido, nos demais casos, cada carácter é
+substituído polo carácter na posición da táboa ASCII que corresponde ao módulo 128 da suma do
+código ASCII do carácter actual máis o do carácter lido previamente. Exemplo: Se tivésemos no
+arquivo orixe:
+35 Abacates
+entón debe aparecer no arquivo de saída:
+3hUa#CDDUYX
+Empregar unha función para o encriptado.*/
+
