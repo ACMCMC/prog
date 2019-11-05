9813bfb05640f54111310f0b43c72457377ad118
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-05 18:31:27 +0100

    Implementado o método avaliar no main do 4

--- boletin3/4.c
+++ boletin3/4.c
@@ -9,58 +9,77 @@ Este programa traballa con polinomios de ata grao 5*/
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct {
+typedef struct
+{
     int grao;
     int coe[6];
 } t_polinomio;
 
-void Crea(int grao, int coe[], t_polinomio *pol) {
+void Crea(int grao, int coe[], t_polinomio *pol)
+{
     pol->grao = grao;
-    for (int i = grao; i >= 0; i--) {
+    for (int i = grao; i >= 0; i--)
+    {
         pol->coe[i] = coe[i];
     }
 }
 
-double Avaliar(t_polinomio pol, double pt) {
+double Avaliar(t_polinomio pol, double pt)
+{
     double valor = 0;
-    for (int i = 0; pol.grao >= i; i++) {
+    for (int i = 0; pol.grao >= i; i++)
+    {
         valor = valor + (pol.coe[i] * pow(pt, i));
     }
     return (valor);
 }
 
-void Mostrar(t_polinomio pol) {
+void Mostrar(t_polinomio pol)
+{
     printf("%d*x^%d ", pol.coe[pol.grao], pol.grao);
-    for (int i = pol.grao - 1; i >= 0; i--) {
+    for (int i = pol.grao - 1; i >= 0; i--)
+    {
         printf("+ %d*x^%d ", pol.coe[i], i);
     }
     printf("\n");
 }
 
-void Derivar(t_polinomio pol, t_polinomio *der) {
+void Derivar(t_polinomio pol, t_polinomio *der)
+{
     der->grao = pol.grao - 1;
-    for (int i = 1; i <= pol.grao; i++) {
+    for (int i = 1; i <= pol.grao; i++)
+    {
         der->coe[i - 1] = pol.coe[i] * i;
     }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     int coe[6], grao;
     printf("Introduza o grao do polinomio: ");
     scanf("%d", &grao);
     printf("\nIntroduza os valores dos coeficientes:\n");
-    for (int i = 0; i <= grao; i++) {
+    for (int i = 0; i <= grao; i++)
+    {
         printf("Coeficiente %d: ", i);
         scanf("%d", &coe[i]);
     }
     t_polinomio miPolinomio;
     Crea(grao, coe, &miPolinomio);
-    printf("O polinomio orixinal e:\n");
+    printf("\nO polinomio orixinal e:\n");
     Mostrar(miPolinomio);
-    printf("O polinomio derivado e:\n");
+    printf("\nO polinomio derivado e:\n");
     t_polinomio der;
     Derivar(miPolinomio, &der);
     Mostrar(der);
+
+    int x = 0;
+    printf("\nIntroduza o valor da x para avaliar o polinomio: ");
+    scanf("%d", &x);
+
+    int aval = Avaliar(miPolinomio, x);
+
+    printf("\nCando x = %d, o polinomio vale: %d",x,aval);
 
     return (EXIT_SUCCESS);
 }
