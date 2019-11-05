48d50c57a053b29cb48839bd9517440e24f0fb71
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-05 11:38:08 +0100

    Acabado o exercicio 8

--- boletin3/8.c
+++ boletin3/8.c
@@ -6,38 +6,58 @@
 #define EXIT_SUCCESS 0
 #define N 30
 
-int Hamming(char *vec1, char *vec2) {
-  if (!(strlen(vec1) == strlen(vec2))) {
+int Hamming(char *vec1, char *vec2)
+{
+  if (!(strlen(vec1) == strlen(vec2)))
+  {
     //printf("Os vectores non se poden comparar porque teñen distinta lonxitude");
-    return(-1);
+    return (-1);
   }
 
   int hamming = 0;
 
-  for (int i = 0; i < strlen(vec1); i++) {
-    if (vec1[i] != vec2[i]) {
+  for (int i = 0; i < strlen(vec1); i++)
+  {
+    if (vec1[i] != vec2[i])
+    {
       hamming++;
     }
   }
   return hamming;
 }
 
-int main() {
+int main()
+{
   FILE *arch;
   char linha[N], contrasena[N];
 
   printf("Introduza o nome do arquivo de contrasenas: ");
   arch = abrir_arquivo("r");
 
+  printf("\nIntroduza a sua contrasena: ");
   getchar();
   gets(contrasena);
 
-  while(feof(arch) == 0) {
-    ler_linha(arch,linha,N);
-    if(Hamming(linha,contrasena) == 0) {
-      printf("Acceso garantido\n");
+char cont_cifrada[N];
+
+  encriptar(contrasena, cont_cifrada);
+
+  int coinciden = 0;
+
+  while (feof(arch) == 0)
+  {
+    ler_linha(arch, linha, N);
+    if (Hamming(linha, cont_cifrada) == 0)
+    {
+      coinciden = 1;
       break;
     }
+  }
+
+  if(coinciden) {
+    printf("\nAcceso garantido\n");
+  } else {
+    printf("\nAcceso denegado\n");
   }
 
   fclose(arch);
