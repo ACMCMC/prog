2970ddb7e20f132eafc790cf2642589c20712c9e
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-05 11:08:32 +0100

    Implementada a función ler_linha

--- boletin3/lib_aldan.h
+++ boletin3/lib_aldan.h
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <errno.h>
 
+void ler_linha(FILE *arquivo, char *cadea_destino, int lonx_cadea) {
+  fgets(cadea_destino,lonx_cadea,arquivo);
+  //printf("%d, %d, %d, %d ",strlen(cadea_destino), cadea_destino[strlen(cadea_destino)], cadea_destino[strlen(cadea_destino)] - 1, cadea_destino[strlen(cadea_destino)] - 2);
+  for(int i = strlen(cadea_destino) - 1; (cadea_destino[i] == '\r') || (cadea_destino[i] == '\n'); i--) {
+    if(cadea_destino[i] == (cadea_destino[i] == '\r') || (cadea_destino[i] == '\n'))
+    cadea_destino[i] = '\0';
+  }
+}
+
 FILE* abrir_arquivo(char *modo)
 {
 
@@ -18,7 +27,7 @@ FILE* abrir_arquivo(char *modo)
     }
     final[4] = '\0';
 
-    if (strcmp(final, ".txt") == 0)
+    if (strcmp(final, ".txt") == 0 && strcmp(final, ".dat") == 0)
     {
       strcat(nome_arquivo, ".txt");
     }
