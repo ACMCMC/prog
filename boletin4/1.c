2319a5de9e05b4d211f1575ae2d97083e3350edf
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-10 01:37:52 +0100

    Documentado o ex. 1

--- boletin4/1.c
+++ boletin4/1.c
@@ -2,6 +2,11 @@
 
 Escribir un programa que declare e inicie (á calquera valor que queirades) un double, un int, e unha cadea. O voso programa entón terá que imprimir o enderezo e o valor almacenado en cada un das variables. Utilizar o especificador de formato "%p" para imprimir os enderezos en notación hexadecimal (base 16). Veredes enderezos algo así como: 0xbfe55918. Os caracteres iniciais 0x indican notación hexadecimal e o resto dos díxitos dan o enderezo. */
 
+/*Autor: Aldán Creo Mariño
+Data: 10 de novembro de 2019
+Versión: 1
+Este programa realiza operacións básicas con direccións de memoria*/
+
 #include <stdio.h>
 
 #define EXIT_SUCCESS 0
