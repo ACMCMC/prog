381ba6f7a4ba35187dea9b6887aaef627bb7be68
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-10 15:06:42 +0100

    Comezado o exercicio 4

---
+++ boletin4/4.c
@@ -0,0 +1,23 @@
+/*ENUNCIADO
+
+Escribir unha función que reciba dúas cadeas de caracteres e devolva outra cadea de caracteres que conteña unicamente os caracteres comúns; de non habelos debe devolver un punteiro a NULL. O prototipo da función debe ser:
+
+    char * CarCompartidos(char *cad1, char *cad2);
+
+Lembrade que a cadea devolta debe levar o carácter de fin de cadea.*/
+
+/*Autor: Aldán Creo Mariño
+Data: 10 de novembro de 2019
+Versión: 1
+Este programa elabora unha cadea de caracteres que contén só os comúns a outras dúas*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EXIT_SUCCESS 0
+
+int main() {
+
+
+    return(EXIT_SUCCESS);
+}
