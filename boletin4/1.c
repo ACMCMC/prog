ce92e8ae3d668fb346e7cb8d6c471173d3d34852
acmc <20495460+ACMCMC@users.noreply.github.com>
2019-11-10 01:36:38 +0100

    Rematado o exercicio 1

---
+++ boletin4/1.c
@@ -0,0 +1,19 @@
+/*ENUNCIADO
+
+Escribir un programa que declare e inicie (á calquera valor que queirades) un double, un int, e unha cadea. O voso programa entón terá que imprimir o enderezo e o valor almacenado en cada un das variables. Utilizar o especificador de formato "%p" para imprimir os enderezos en notación hexadecimal (base 16). Veredes enderezos algo así como: 0xbfe55918. Os caracteres iniciais 0x indican notación hexadecimal e o resto dos díxitos dan o enderezo. */
+
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+
+int main() {
+    double a = 3.1415;
+    int b = 1492;
+    char c[] = "Hola";
+
+    printf("%lf esta gardado en 0x%p\n",a,&a);
+    printf("%d esta gardado en 0x%p\n",b,&b);
+    printf("%s esta gardado en 0x%p\n",c,&c);
+
+    return(EXIT_SUCCESS);
+}
