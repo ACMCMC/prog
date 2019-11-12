9fe02c45c5b1f998d7f674aa0928dfa49f83745a
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-11-12 10:42:55 +0000

    Feita a funcion de ordear no 4

--- boletin4/4.c
+++ boletin4/4.c
@@ -13,8 +13,53 @@ Este programa elabora unha cadea de caracteres que contén só os comúns a outr
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define EXIT_SUCCESS 0
+
+char *Ordear(char *cadea)
+{
+    char caracter_swap;
+    char *cadea_ordeada = (char *)malloc(sizeof(*cadea)*(strlen(cadea) + 1));
+    int correcto, i;
+
+    strcpy(cadea_ordeada,cadea);
+
+    do
+    {
+        correcto = 1;
+        for (i = 1; cadea_ordeada[i] != '\0'; i++)
+        {
+            if (cadea_ordeada[i] < cadea_ordeada[i - 1])
+            {
+                caracter_swap = cadea_ordeada[i - 1];
+                cadea_ordeada[i - 1] = cadea_ordeada[i];
+                cadea_ordeada[i] = caracter_swap;
+                correcto = 0;
+            }
+        }
+    } while (!correcto);
+
+    int shift = 0;
+
+
+    for (i = 1; cadea_ordeada[i] != '\0'; i++)
+    {
+
+        if (cadea_ordeada[i] == cadea_ordeada[i - 1])
+        {
+            shift++;
+        }
+        else
+        {
+            cadea_ordeada[i - shift] = cadea_ordeada[i];
+        }
+    }
+
+    cadea_ordeada[i-shift] = '\0';
+
+    return (cadea_ordeada);
+}
 
 char *CarCompartidos(char *cad1, char *cad2)
 {
@@ -22,23 +67,30 @@ char *CarCompartidos(char *cad1, char *cad2)
 
     int cuenta = 0, shift = 0, correcto, i;
 
-    do {
-    correcto = 1;
-    for(i = 1; cad1[i] != '\0'; i++) {
-        if(cad1[i] < cad1[i-1]) {
-            cad1[i-shift] = cad1[i-1];
-            cad1[i-shift-1] = cad1[i];
-            correcto = 0;
-        } else if (cad1[i] == cad1[i-1]) {
-            
-            shift++;
-        } else {
-            cad1_ord[i-shift] = cad1[i];
-        }
-    }
-    } while(!correcto);
+    do
+    {
+        correcto = 1;
+        for (i = 1; cad1[i] != '\0'; i++)
+        {
+            if (cad1[i] < cad1[i - 1])
+            {
+                cad1[i - shift] = cad1[i - 1];
+                cad1[i - shift - 1] = cad1[i];
+                correcto = 0;
+            }
+            else if (cad1[i] == cad1[i - 1])
+            {
 
-    printf("%s",cad1);
+                shift++;
+            }
+            else
+            {
+                cad1[i - shift] = cad1[i];
+            }
+        }
+    } while (!correcto);
+
+    printf("%s", cad1);
 
     while ((*cad1 != '\0') && (*cad2 != '\0'))
     {
@@ -60,8 +112,8 @@ char *CarCompartidos(char *cad1, char *cad2)
 int main()
 {
 
-    printf("Iniciando busqueda...");
-    printf("\n%s", CarCompartidos("dcba", "Troal"));
+    printf("Iniciando busqueda...\n");
+    printf("\n%s", Ordear("dcbaaaui"));
     printf("\nFin da busqueda.");
 
     return (EXIT_SUCCESS);
