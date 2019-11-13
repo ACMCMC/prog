9c32e39cdeb0e75ff5cb6fd90048955c129c5aca
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-11-13 17:56:02 +0000

    Creada a funcion para crear vectores

--- boletin4/5.c
+++ boletin4/5.c
@@ -16,11 +16,13 @@ Este programa elabora unha cadea de caracteres que contén só os comúns a outr
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <errno.h>
 
 #define EXIT_SUCCESS 0
 
-char *corrixirNome(char *nome_arq) { //Esta funcion toma unha cadea de caracteres e comproba se acaba en ".dat"; se non, añadelle a extension e devolve a cadea corrixida
+char *corrixirNome(char *nome_arq)
+{ //Esta funcion toma unha cadea de caracteres e comproba se acaba en ".dat"; se non, añadelle a extension e devolve a cadea corrixida
 
     if (strlen(nome_arq) > 4) //Se a cadea non mide mais de 4 caracteres, non pode ser algo seguido de ".dat"
     {
@@ -45,7 +47,7 @@ char *corrixirNome(char *nome_arq) { //Esta funcion toma unha cadea de caractere
     return nome_arq; //Devolvemos a cadea corrixida
 }
 
-int *suma(int tam_vec, int *v1, int *v2)
+int *suma(int tam_vec, int *v1, int *v2) //Sumamos os dous vectores
 {
     int *suma = (int *)malloc(sizeof(*v1) * tam_vec);
     for (int i = 0; i < tam_vec; i++)
@@ -59,24 +61,127 @@ int main(int argc, char **argv)
 {
     if (argc < 2) //Non se especificou un nome de ficheiro como argumento
     {
-        printf("Precisase o nome do arquivo a abrir como un parametro do programa. Para obter axuda, escriba \"-help\"\n");
+        printf("Precisase o nome do arquivo a abrir como un parametro do programa.\n\nPara obter axuda, use \"-help\".\n");
         exit(1);
     }
 
     if (/*(argv[1][0] == '\?') ||*/ (strcmp(argv[1], "-help") == 0)) //O usuario introduce -help como argumento
     {
-        printf("Para utilizar o programa, introduza o nome do arquivo binario que conten os datos dos vectores. Este arquivo debese atopar na carpeta de execucion do programa, e ter permisos de lectura. O arquivo debera ter extension \".dat\"\n");
+        printf("Para utilizar o programa, introduza o nome do arquivo binario que conten os datos dos vectores. Este arquivo debese atopar na carpeta de execucion do programa, e ter permisos de lectura. O arquivo debera ter extension \".dat\".\n\nPode inicializar un arquivo cos datos de novos vectores co parametro \"-create_file\" (nome do arquivo).\n");
         exit(EXIT_SUCCESS);
-    } else if (strcmp(argv[1],"-create_file") == 0) {
-        if ((argc > 2) && (argv[2][0] != '-')) {
+    }
+    else if (strcmp(argv[1], "-create_file") == 0)
+    {
+        if ((argc > 2) && (argv[2][0] != '-'))
+        { //Se se introduciu "-create_file" e o segundo argumento non empeza con -
+
+            char *nome_corrixido = corrixirNome(argv[2]); //Miramos se o nome do arquivo acaba en ".dat"
+
+            printf("Creando: %s\n", nome_corrixido);
+            FILE *arq = fopen(nome_corrixido, "wb");
+            if (arq == NULL)
+            {
+                printf("\nNon se puido crear o arquivo (%p - %s)\n", errno, strerror(errno));
+                exit(1);
+            }
+
+            int *cadea1, *cadea2;
+            int lonx_cadea1 = 0, lonx_cadea2 = 0;
+
+            cadea1 = malloc(0);
+            cadea2 = malloc(0);
+
+            char caracter_actual;
+            int num_actual, i;
+
+            printf("\nCreando o vector 1...");
+
+            do
+            {
+                lonx_cadea1++;
+                printf("\nIntroduza o dato %d: ", lonx_cadea1);
+                num_actual = 0;
+                i = 0;
+                caracter_actual = '0';
+                do
+                {
+                    num_actual = num_actual * 10 + (caracter_actual - '0');
+                    caracter_actual = getchar();
+                    //printf("\n\tCaracter: %c, suma: %d", caracter_actual, num_actual);
+                    i++;
+                } while ((caracter_actual > '0') && (caracter_actual < '9'));
+
+                cadea1 = realloc(cadea1, sizeof(*cadea1) * lonx_cadea1);
+                cadea1[lonx_cadea1 - 1] = num_actual;
+
+            } while ((caracter_actual != '\n') || (i > 1));
+
+            lonx_cadea1--;
+            cadea1 = realloc(cadea1, sizeof(*cadea1) * lonx_cadea1);
+
+            printf("Crearase o vector: (");
+            for (i = 0; i < (lonx_cadea1 - 1); i++)
+            {
+                printf(" %d ,", cadea1[i]);
+            }
+            printf(" %d )\n", cadea1[lonx_cadea1 - 1]);
+
+            printf("\nCreando o vector 2...");
+
+            do
+            {
+                lonx_cadea2++;
+                printf("\nIntroduza o dato %d: ", lonx_cadea2);
+                num_actual = 0;
+                i = 0;
+                caracter_actual = '0';
+                do
+                {
+                    num_actual = num_actual * 10 + (caracter_actual - '0');
+                    caracter_actual = getchar();
+                    //printf("\n\tCaracter: %c, suma: %d", caracter_actual, num_actual);
+                    i++;
+                } while ((caracter_actual > '0') && (caracter_actual < '9'));
+
+                cadea2 = realloc(cadea2, sizeof(*cadea2) * lonx_cadea2);
+                cadea2[lonx_cadea2 - 1] = num_actual;
+
+            } while ((caracter_actual != '\n') || (i > 1));
+
+            lonx_cadea2--;
+            cadea2 = realloc(cadea2, sizeof(*cadea2) * lonx_cadea2);
+
+            printf("Crearase o vector: (");
+            for (i = 0; i < (lonx_cadea2 - 1); i++)
+            {
+                printf(" %d ,", cadea2[i]);
+            }
+            printf(" %d )\n", cadea2[lonx_cadea2 - 1]);
+
+
+
+            fwrite(&lonx_cadea1, sizeof(lonx_cadea1), 1, arq);
+            fwrite(cadea1, sizeof(*cadea1), lonx_cadea1, arq);
+            fwrite(&lonx_cadea2, sizeof(lonx_cadea2), 1, arq);
+            fwrite(cadea2, sizeof(*cadea2), lonx_cadea2, arq);
+
+            fclose(arq);
 
             exit(EXIT_SUCCESS);
-        } else {
+        }
+        else
+        {
+            printf("Sintaxe non admitida.\n\n\tUso: -create_file (nome do arquivo) \n\nPara obter axuda, use \"-help\".\n");
             exit(1);
         }
     }
+    else if (argv[1][0] == '-')
+    {
+        printf("Sintaxe non admitida.\n\nPara obter axuda, use \"-help\".\n");
+        exit(1);
+    }
 
-    char *nome_corrixido = corrixirNome(argv[1]);
+    char *nome_corrixido = corrixirNome(argv[1]); //Miramos se o nome do arquivo acaba en ".dat"
 
     printf("Abrindo: %s\n", nome_corrixido);
     FILE *arq = fopen(nome_corrixido, "rb");
@@ -86,37 +191,7 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    char *cadea1, *cadea2;
-    int lonx_cadea1 = 0, lonx_cadea2 = 0;
-
-    cadea1 = malloc(0);
-    cadea2 = malloc(0);
-
-    printf("\nIntroduza a sua primeira cadea: ");
-
-    do
-    {
-        lonx_cadea1++;
-        cadea1 = realloc(cadea1, sizeof(*cadea1) * lonx_cadea1);
-        scanf("%d", cadea1[lonx_cadea1 - 1]);
-    } while (cadea1[lonx_cadea1 - 1] != '\n');
-
-    cadea1[lonx_cadea1 - 1] = '\0';
-
-    printf("\nLido: %s\n", cadea1);
-
-    printf("\nIntroduza a sua segunda cadea: ");
-
-    do
-    {
-        lonx_cadea2++;
-        cadea2 = realloc(cadea2, sizeof(*cadea2) * lonx_cadea2);
-        cadea2[lonx_cadea2 - 1] = getchar();
-    } while (cadea2[lonx_cadea2 - 1] != '\n');
-
-    cadea2[lonx_cadea2 - 1] = '\0';
-
-    printf("\nLido: %s\n", cadea2);
+    fclose(arq);
 
     return (EXIT_SUCCESS);
 }
