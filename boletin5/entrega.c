d46abd692946d8f1333f9865a6ccd6e0a5f8c2e6
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-11-25 16:45:25 +0100

    Progresos no 5

--- boletin5/entrega.c
+++ boletin5/entrega.c
@@ -5,5 +5,14 @@
 #define EXIT_SUCCESS 0
 
 int main(){
+    bignum a;
+    a = str2bignum("98765432109876543210");
+
+    for (int i = 0; i < a.tam; i++)
+    {
+        printf("%d\t",a.val[i]);
+    }
+    
+
     return(EXIT_SUCCESS);
 }
--- boletin5/lib_aldan.h
+++ boletin5/lib_aldan.h
@@ -2,126 +2,179 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <math.h>
 
-typedef struct {
+typedef struct
+{
     int signo;
     unsigned int tam;
     unsigned char *val;
 } bignum;
 
-void invertir_signo(bignum *n) {
-    if(n->signo == 0) {
+void invertir_signo(bignum *n)
+{
+    if (n->signo == 0)
+    {
         n->signo = -1;
-    } else {
+    }
+    else
+    {
         n->signo = 0;
     }
 }
 
-int comparar_magnitud(bignum a, bignum b) {
-    if (a.tam > b.tam) {
+int comparar_magnitud(bignum a, bignum b) //Devuelve 1 si el valor absoluto de a es mayor que el de b, -1 si es menor, y 0 si son iguales
+{
+    if (a.tam > b.tam)
+    {
         return 1;
-    } else if (b.tam > a.tam) {
+    }
+    else if (b.tam > a.tam)
+    {
         return -1;
-    } else if (a.val > b.val) {
+    }
+    else if (a.val > b.val)
+    {
         return 1;
-    } else if (b.val > a.val) {
+    }
+    else if (b.val > a.val)
+    {
         return -1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-int comparar(bignum a, bignum b) {
+int comparar(bignum a, bignum b)
+{
     if (a.signo != b.signo)
     {
-        if(a.signo == 0) {
+        if (a.signo == 0)
+        {
             return 1;
-        } else {
+        }
+        else
+        {
             return -1;
         }
-    } else if (a.tam > b.tam) {
+    }
+    else if (a.tam > b.tam)
+    {
         return 1;
-    } else if (b.tam > a.tam) {
+    }
+    else if (b.tam > a.tam)
+    {
         return -1;
-    } else if (a.val > b.val) {
+    }
+    else if (a.val > b.val)
+    {
         return 1;
-    } else if (b.val > a.val) {
+    }
+    else if (b.val > a.val)
+    {
         return -1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-bignum str2bignum(char *str) {
+bignum str2bignum(char *str)
+{
     bignum num;
-    int i;
-    if(strlen(str) < 1) {
+    int i = 0, n = 0, conv = 0;
+    if (strlen(str) < 1)
+    {
         exit(EXIT_FAILURE);
     }
 
-    if(*str == '-') {
+    if (*str == '-')
+    {
         num.signo = -1;
-        i = 1;
-    } else {
-        num.signo = 0;
-        i = 0;
+        str++; //diminuímos a lonxitude da cadea en unha unidade
     }
-while (i < strlen(str)) {
-    i++;
-};
-return num;
+    else
+    {
+        num.signo = 0;
+    }
+
+    num.val = NULL; //en principio, non temos valores para o número
+
+    i = strlen(str); //imos empezar a ler a cadea dende o final para pasala a base 256
+
+    while (i >= 0)
+    {
+        conv = conv + pow(10,i) * (str[i] - '0');
+        if (conv > UCHAR_MAX){
+            num.tam++;
+            num.val = (unsigned char *)realloc(num.val, sizeof(unsigned char)*num.tam);
+            num.val[num.tam-1] = conv % UCHAR_MAX; //Gardamos o resto
+            conv = conv / UCHAR_MAX; //Como conv vai ser o noso seguinte byte, facemos a division enteira e gardamos o resultado como valor de partida
+            n = 0;
+        }
+        i--;
+        n++;
+    }
+    
+    return num;
 }
 
-bignum add(bignum a, bignum b) {
+bignum add(bignum a, bignum b)
+{
     bignum res;
     unsigned short res_temp;
 
-    int i;
+    int i, carry = 0;
 
-    for(i = 0; (i<a.tam) && (i<b.tam); i++) {
-
-    res_temp = ((unsigned short) a.val[i]) + ((unsigned short) b.val[i]);
-    if(res_temp > USHRT_MAX) {
-        res.val[i] = (unsigned char) (res_temp - USHRT_MAX);
+    if (comparar_magnitud(a, b) < 0)
+    {
+        res.tam = b.tam;
+    }
+    else
+    {
+        res.tam = a.tam;
     }
 
-    }
+    res.val = (unsigned char *)malloc(sizeof(unsigned char) * res.tam);
 
-    if(comparar_magnitud(a,b) >= 1) {
-        res.val[0] = a.val[0]+b.val[0];
-        for(res.tam = 1; res.tam <= a.tam; res.tam++) {
-        res.val[res.tam] = a.val[res.tam]+b.val[res.tam];
-            if(sum_has_carry(a.val[res.tam-1],b.val[res.tam-1])) { //si el int actual es uint_max, no le vamos a poder sumar el carry anterior
-                res.val[i] = res.val[i] + 1;
-            }
+    for (i = 0; (i < a.tam) && (i < b.tam); i++)
+    {
+
+        res_temp = ((unsigned short)a.val[i]) + ((unsigned short)b.val[i]) + carry;
+        if (res_temp > USHRT_MAX)
+        {
+            res.val[i] = (unsigned char)(res_temp - USHRT_MAX);
+            carry = 1;
+        }
+        else
+        {
+            carry = 0;
+            res.val[i] = (unsigned char)res_temp;
         }
     }
 
-}
-return res;
-    } else {
-        invertir_signo(&b);
-        res = sub(a, b);
-        return res;
-    }
+    return res;
 }
 
-bignum sub(bignum a, bignum b) {
-
+/*bignum sub(bignum a, bignum b)
+{
 }
 
-bignum mult(bignum a, bignum b) {
-
+bignum mult(bignum a, bignum b)
+{
 }
 
-bignum remainder(bignum a, bignum b) {
-
+bignum remainder(bignum a, bignum b)
+{
 }
 
-bignum fact(bignum n) {
-
+bignum fact(bignum n)
+{
 }
 
-bignum div(bignum a, bignum b, bignum n) {
-
-}
+bignum div(bignum a, bignum b, bignum n)
+{
+}*/
