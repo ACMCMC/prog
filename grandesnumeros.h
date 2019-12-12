5ba7b860ccc3f4c3dd4aa42ac9d37dfba99ffd33
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-12-12 16:09:17 +0000

    Cambios de formato

--- grandesnumeros.h
+++ grandesnumeros.h
@@ -5,11 +5,12 @@
 //!!!ALGÚNS NOMES DAS FUNCIÓNS FORON MODIFICADOS POR INTERFERIR CON NOMES DAS LIBREÍAS STANDARD
 #ifndef GRANDESNUMEROS_H
 #define GRANDESNUMEROS_H
-typedef struct {
- int sign;
- int tam;
- int *val;
-}bignum;
+typedef struct
+{
+    int sign;
+    int tam;
+    int *val;
+} bignum;
 
 //Precondicións: pasarlle unha cadena de caracteres do 0-9 de calquera lonxitude non nula, con signo e sen parte decimal, cun \n ao final.
 bignum str2bignum(char *str);
@@ -22,16 +23,16 @@ bignum add(bignum a, bignum b);
 //e co signo adecuado (se a operación equivale a unha resta chamamarase automáticamente).
 
 //Precondicións: dous bignums de calquer signo e tamaño, sen ceros á esquerda no valor.
-bignum resta(bignum a,bignum b);
-//Poscondicións: un bignum correspondente á resta das entradas (a)-(b) de tamaño menor ou igual ao maior se os signos son iguais 
+bignum resta(bignum a, bignum b);
+//Poscondicións: un bignum correspondente á resta das entradas (a)-(b) de tamaño menor ou igual ao maior se os signos son iguais
 //e de tamaño maior ou igual se non o son; co signo adecuado (se a operación equivale a unha suma chamamarase automáticamente).
 
 //Precondicións: dous bignums de calquer signo e tamaño.
-bignum mult(bignum a,bignum b);
+bignum mult(bignum a, bignum b);
 //Poscondicións: un bignum de tamaño maior ou igual ao correspondente ao producto das entradas co signo adecuado.
 
 //Precondicións: dous bignums de calquer tamaño e signo sen ceros á esquerda no valor; o bignum (n) NON pode ser NULO.
-bignum modulo(bignum a,bignum n);
+bignum modulo(bignum a, bignum n);
 //Poscondicións: un bignum de lonxitude igual ou menor a (a) que corresponde co resultado de a%n.
 
 //Precondicións: un bignum de signo positivo, sen ceros á esquerda no valor, igual ou maior a 0; do contrario abortarase o programa.
@@ -39,8 +40,7 @@ bignum fact(bignum n);
 //Poscondicións: o factorial do número, tendo en conta que 0!=1.
 
 //Precondicións: dous bignums de calquer tamaño e signo; e un bignum (n) que NON sexa NULO e sen ceros á esquerda no valor.
-bignum dividir(bignum a,bignum b,bignum n);
+bignum dividir(bignum a, bignum b, bignum n);
 //Un bignum de lonxitude igual ou menor a (a)*(b) que corresponde co resultado de (a*b)%n.
 
-#endif /* GRANDESNUMEROS_H */
-
+#endif
--- main.c
+++ main.c
@@ -8,138 +8,178 @@
 #include <string.h>
 #include "grandesnumeros.h"
 
-int main(int argc, char** argv) {
-    char operacion,letra,*valor1,*valor2,*valor3,*out;
-    int lonx,limpar=1;
-    bignum num1,num2,num3,result;
+int main(int argc, char **argv)
+{
+    char operacion, letra, *valor1, *valor2, *valor3, *out;
+    int lonx, limpar = 1;
+    bignum num1, num2, num3, result;
     printf("Seleccione a operación:\n   S - Suma\n   R - Resta\n   P - Producto\n   M - Módulo\n   F - Factorial\n   D - Dividir\nOperación: ");
-    scanf("%c",&operacion);
+    scanf("%c", &operacion);
     //Preparamos os números
-    if(operacion=='s'||operacion=='S'||operacion=='r'||operacion=='R'||operacion=='p'||operacion=='P'||operacion=='m'||operacion=='M'){
+    if (operacion == 's' || operacion == 'S' || operacion == 'r' || operacion == 'R' || operacion == 'p' || operacion == 'P' || operacion == 'm' || operacion == 'M')
+    {
         //PRIMEIRO NUMERO
         printf("Introduza a lonxitude máxima dos números: ");
-        scanf("%d",&lonx);
+        scanf("%d", &lonx);
         getchar();
-        lonx = lonx+2;
-        valor1 = (char*)malloc(sizeof(char)*(lonx));
+        lonx = lonx + 2;
+        valor1 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o primeiro número: ");
-        fgets(valor1,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor1[i]=='\n'){
-                valor1[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor1, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor1[i] == '\n')
+            {
+                valor1[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor1[0]!='-' && valor1[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor1[i]=valor1[i-1];
-            valor1[0]='+';
+        if (valor1[0] != '-' && valor1[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor1[i] = valor1[i - 1];
+            valor1[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
         //SEGUNDO NUMERO
-        limpar=1;
-        valor2 = (char*)malloc(sizeof(char)*(lonx));
+        limpar = 1;
+        valor2 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o segundo número: ");
-        fgets(valor2,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor2[i]=='\n'){
-                valor2[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor2, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor2[i] == '\n')
+            {
+                valor2[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor2[0]!='-' && valor2[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor2[i]=valor2[i-1];
-            valor2[0]='+';
+        if (valor2[0] != '-' && valor2[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor2[i] = valor2[i - 1];
+            valor2[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
         num1 = str2bignum(valor1);
         num2 = str2bignum(valor2);
         free(valor1);
         free(valor2);
     }
-    else if(operacion=='f'||operacion=='F'){
+    else if (operacion == 'f' || operacion == 'F')
+    {
         //PRIMEIRO NUMERO
         printf("Introduza a lonxitude do número: ");
-        scanf("%d",&lonx);
+        scanf("%d", &lonx);
         getchar();
-        lonx = lonx+2;
-        valor1 = (char*)malloc(sizeof(char)*(lonx));
+        lonx = lonx + 2;
+        valor1 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o primeiro número: ");
-        fgets(valor1,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor1[i]=='\n'){
-                valor1[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor1, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor1[i] == '\n')
+            {
+                valor1[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor1[0]!='-' && valor1[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor1[i]=valor1[i-1];
-            valor1[0]='+';
+        if (valor1[0] != '-' && valor1[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor1[i] = valor1[i - 1];
+            valor1[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
     }
-    else{
+    else
+    {
         //PRIMEIRO NUMERO
         printf("Introduza a lonxitude máxima dos números: ");
-        scanf("%d",&lonx);
+        scanf("%d", &lonx);
         getchar();
-        lonx = lonx+2;
-        valor1 = (char*)malloc(sizeof(char)*(lonx));
+        lonx = lonx + 2;
+        valor1 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o primeiro número: ");
-        fgets(valor1,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor1[i]=='\n'){
-                valor1[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor1, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor1[i] == '\n')
+            {
+                valor1[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor1[0]!='-' && valor1[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor1[i]=valor1[i-1];
-            valor1[0]='+';
+        if (valor1[0] != '-' && valor1[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor1[i] = valor1[i - 1];
+            valor1[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
         //SEGUNDO NUMERO
-        limpar=1;
-        valor2 = (char*)malloc(sizeof(char)*(lonx));
+        limpar = 1;
+        valor2 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o segundo número: ");
-        fgets(valor2,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor2[i]=='\n'){
-                valor2[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor2, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor2[i] == '\n')
+            {
+                valor2[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor2[0]!='-' && valor2[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor2[i]=valor2[i-1];
-            valor2[0]='+';
+        if (valor2[0] != '-' && valor2[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor2[i] = valor2[i - 1];
+            valor2[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
         //Terceiro NUMERO
-        limpar=1;
-        valor3 = (char*)malloc(sizeof(char)*(lonx));
+        limpar = 1;
+        valor3 = (char *)malloc(sizeof(char) * (lonx));
         printf("Introduza o segundo número: ");
-        fgets(valor3,lonx,stdin);
-        for(int i=0;i<lonx;i++){
-            if(valor3[i]=='\n'){
-                valor3[i]='\0'; //Se o número introducido foi menor desplazamos o final da cadea
-                limpar = 0; //Se este é o caso non fai falta limpar o stdin
+        fgets(valor3, lonx, stdin);
+        for (int i = 0; i < lonx; i++)
+        {
+            if (valor3[i] == '\n')
+            {
+                valor3[i] = '\0'; //Se o número introducido foi menor desplazamos o final da cadea
+                limpar = 0;       //Se este é o caso non fai falta limpar o stdin
             }
         }
-        if(valor3[0]!='-' && valor3[0]!='+'){ //Se o usuario non introduciu signo, o programa introduce un +
-            for(int i=lonx-2;i>0;i--) valor3[i]=valor3[i-1];
-            valor3[0]='+';
+        if (valor3[0] != '-' && valor3[0] != '+')
+        { //Se o usuario non introduciu signo, o programa introduce un +
+            for (int i = lonx - 2; i > 0; i--)
+                valor3[i] = valor3[i - 1];
+            valor3[0] = '+';
         }
-        if(limpar==1){
-            while(getchar()!='\n'); //Limpamos o buffer se queda algún caracter
+        if (limpar == 1)
+        {
+            while (getchar() != '\n')
+                ; //Limpamos o buffer se queda algún caracter
         }
         num1 = str2bignum(valor1);
         num2 = str2bignum(valor2);
@@ -148,28 +188,41 @@ int main(int argc, char** argv) {
         free(valor2);
         free(valor3);
     }
-    switch(operacion){
-        case 'S':
-        case 's': result = add(num1,num2); break;
-        case 'R':
-        case 'r': result = resta(num1,num2); break;
-        case 'p':
-        case 'P': result = mult(num1,num2); break;
-        case 'M':
-        case 'm': result = modulo(num1,num2); break;
-        case 'F':
-        case 'f': result = fact(num1); break;
-        case 'D':
-        case'd': result = dividir(num1,num2,num3); break;
-        default:
-            printf("Non se recoñece ese comando");
-            return (EXIT_FAILURE);
+    switch (operacion)
+    {
+    case 'S':
+    case 's':
+        result = add(num1, num2);
+        break;
+    case 'R':
+    case 'r':
+        result = resta(num1, num2);
+        break;
+    case 'p':
+    case 'P':
+        result = mult(num1, num2);
+        break;
+    case 'M':
+    case 'm':
+        result = modulo(num1, num2);
+        break;
+    case 'F':
+    case 'f':
+        result = fact(num1);
+        break;
+    case 'D':
+    case 'd':
+        result = dividir(num1, num2, num3);
+        break;
+    default:
+        printf("Non se recoñece ese comando");
+        return (EXIT_FAILURE);
     }
     //Imprimir resultado
     printf("Resultado: ");
-    if(result.sign==1) printf("-");
-    for (int i=result.tam-1;i>=0;i--) printf("%d",result.val[i]);
+    if (result.sign == 1)
+        printf("-");
+    for (int i = result.tam - 1; i >= 0; i--)
+        printf("%d", result.val[i]);
     return (EXIT_SUCCESS);
 }
-
-    
