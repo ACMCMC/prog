66234a9e2485614c9eb234ee026709d7c28740a7
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-12-16 16:52:46 +0000

    Cambios no test

--- boletin5/test.c
+++ boletin5/test.c
@@ -6,7 +6,7 @@ int main() {
     int i, tam_errores = 0, tam_str = 1;
     char *str, **errores = NULL;
 
-    for( (i = 0) ; i < 500 ; ( i++ , (str[tam_str-1] == '9') ? () : () ) ) ( (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( printf(" correcto\n") ) : ( printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) , tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) ) );
+    for( (i = 0) ; i < 500 ; ( i++ , (str[tam_str-1] == '9') ? () : () ) ) ( (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( printf(" correcto\n") ) : ( printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) , tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) , memmove(errores[tam_errores-1] , str , tam_str) ) );
 
     return 0;
 }
