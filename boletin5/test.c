4db1a4b11462234531192e1c2253f509c4dbe1b9
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-12-16 17:18:54 +0000

    Cambios no test para que non mostre os casos de num >= 100

--- boletin5/test.c
+++ boletin5/test.c
@@ -11,7 +11,7 @@ int main() {
     str[1] = '\0';
 
     for( (i = 0) ; i < 50000 ; ( i++ , (str[tam_str-1] == '9') ? ( tam_str++ , str = realloc(str,sizeof(*str)*(tam_str+1)) , str[tam_str-1] = '0' , str[tam_str] = '\0' ) : ( str[tam_str-1]++ ) ) ) ( /*printf("Comprobando %s:",str) ,*/ (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( /*printf(" correcto\n") ,*/ NULL ) : ( /*printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) ,*/ tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) , memcpy(errores[tam_errores-1] , str , sizeof(*str)*(tam_str+1)) ) );
-    for ( ( printf("\n\nRESUMO DE ERROS:\n\nComprobados %d casos. %d erros atopados (%f\%)\n\n",i,tam_errores,100.0*(((float)tam_errores)/((float)i))) , i = 0 ) ; i < tam_errores ; i++ ) ( printf("%d.\t%s\t-->   %s\n",i,errores[i],bignum2str(str2bignum(errores[i]))) );
+    for ( ( printf("\n\nRESUMO DE ERROS:\n\nComprobados %d casos. %d erros atopados (%f\%)\n\n",i,tam_errores,100.0*(((float)tam_errores)/((float)i))) , i = 0 ) ; i < ( (tam_errores < 100) ? tam_errores : 1) ; i++ ) ( (tam_errores < 100) ? (printf("%d.\t%s\t-->   %s\n",i,errores[i],bignum2str(str2bignum(errores[i])))) : ( printf("O numero de casos para mostrar e demasiado grande.\n") ) );
 
     return 0;
 }
