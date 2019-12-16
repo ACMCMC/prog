366cf1f485e32a892f0c7f5d98f770a172e05ac9
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-12-16 17:12:17 +0000

    Incrementado o numero de tests

--- boletin5/test.c
+++ boletin5/test.c
@@ -10,7 +10,7 @@ int main() {
     str[0] = '1';
     str[1] = '\0';
 
-    for( (i = 0) ; i < 500 ; ( i++ , (str[tam_str-1] == '9') ? ( tam_str++ , str = realloc(str,sizeof(*str)*(tam_str+1)) , str[tam_str-1] = '0' , str[tam_str] = '\0' ) : ( str[tam_str-1]++ ) ) ) ( printf("Comprobando %s:",str) , (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( printf(" correcto\n") ) : ( printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) , tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) , memcpy(errores[tam_errores-1] , str , sizeof(*str)*(tam_str+1)) ) );
+    for( (i = 0) ; i < 50000 ; ( i++ , (str[tam_str-1] == '9') ? ( tam_str++ , str = realloc(str,sizeof(*str)*(tam_str+1)) , str[tam_str-1] = '0' , str[tam_str] = '\0' ) : ( str[tam_str-1]++ ) ) ) ( /*printf("Comprobando %s:",str) ,*/ (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( /*printf(" correcto\n") ,*/ NULL ) : ( /*printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) ,*/ tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) , memcpy(errores[tam_errores-1] , str , sizeof(*str)*(tam_str+1)) ) );
     for ( ( printf("\n\nRESUMO DE ERROS:\n\nComprobados %d casos. %d erros atopados (%f\%)\n\n",i,tam_errores,100.0*(((float)tam_errores)/((float)i))) , i = 0 ) ; i < tam_errores ; i++ ) ( printf("%d.\t%s\t-->   %s\n",i,errores[i],bignum2str(str2bignum(errores[i]))) );
 
     return 0;
