e89b61aa6ed2d98ceedf9cdb6436afd5590e14c6
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2019-12-16 16:48:51 +0000

    Cambios no test

--- boletin5/test.c
+++ boletin5/test.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lib_bignum.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    bignum num = str2bignum("453453453");
-    printf("Tamano: %d.\n",num.tam);
-    for (int i = 0; i < num.tam; i++) {
-        printf("Numero %d: %d\n",i,num.val[i]);
-    }
-    printf("Fin do numero.\n");
-    printf("%s\n",bignum2str(num));
-    return (0);
+    int i, tam_errores = 0, tam_str = 1;
+    char *str, **errores = NULL;
+
+    for( (i = 0) ; i < 500 ; ( i++ , (str[tam_str-1] == '9') ? () : () ) ) ( (strcmp(bignum2str(str2bignum(str)) , str) == 0) ? ( printf(" correcto\n") ) : ( printf(" INCORRECTO, devolto %s\n",bignum2str(str2bignum(str))) , tam_errores++ , errores = realloc(errores, sizeof(*errores)*tam_errores ) , errores[tam_errores-1] = malloc( sizeof(*str)*(tam_str+1) ) ) );
+
+    return 0;
 }
