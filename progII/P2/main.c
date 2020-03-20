0ec83a0412d7ec3fc8b0d4812f1d8f570ba38f2d
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-03-20 20:28:21 +0100

    Arreglado el formato

--- progII/P2/main.c
+++ progII/P2/main.c
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #define BONIFICACION 0.6
 
-typedef enum {TRUE = 1, FALSE = 0} booleano;
+typedef enum
+{
+    TRUE = 1,
+    FALSE = 0
+} booleano;
 
-TIPOELEMENTOLISTA totalBonificaciones(TLISTA listaBonificaciones) {
+TIPOELEMENTOLISTA totalBonificaciones(TLISTA listaBonificaciones)
+{
     TNODOLISTA nodoSiguiente = primero(listaBonificaciones);
-    TIPOELEMENTOLISTA suma = (TIPOELEMENTOLISTA) 0, temporal;
-    while (nodoSiguiente != fin(listaBonificaciones)) {
+    TIPOELEMENTOLISTA suma = (TIPOELEMENTOLISTA)0, temporal;
+    while (nodoSiguiente != fin(listaBonificaciones))
+    {
         recupera(listaBonificaciones, nodoSiguiente, &temporal);
         suma += temporal;
         nodoSiguiente = siguiente(listaBonificaciones, nodoSiguiente);
@@ -17,76 +23,100 @@ TIPOELEMENTOLISTA totalBonificaciones(TLISTA listaBonificaciones) {
     return (suma);
 }
 
-void recaudarBonificaciones(TLISTA *listaBonificaciones, TIPOELEMENTOLISTA dineroARecaudar, TIPOELEMENTOLISTA *totalBonificacionesRecaudadas) {
+void recaudarBonificaciones(TLISTA *listaBonificaciones, TIPOELEMENTOLISTA dineroARecaudar, TIPOELEMENTOLISTA *totalBonificacionesRecaudadas)
+{
     TNODOLISTA nodoSiguiente;
     TNODOLISTA nodoDelMenorElemento;
     TIPOELEMENTOLISTA elemento;
     TIPOELEMENTOLISTA menorElemento;
-    if (totalBonificaciones(*listaBonificaciones) >= dineroARecaudar) { //hay suficiente dinero para recaudar la cantidad introducida
+    if (totalBonificaciones(*listaBonificaciones) >= dineroARecaudar)
+    { //hay suficiente dinero para recaudar la cantidad introducida
         *totalBonificacionesRecaudadas += dineroARecaudar;
-        while (dineroARecaudar > 0) {
+        while (dineroARecaudar > 0)
+        {
             nodoSiguiente = primero(*listaBonificaciones);
             nodoDelMenorElemento = nodoSiguiente;
             recupera(*listaBonificaciones, nodoDelMenorElemento, &menorElemento);
-            while (nodoSiguiente != fin(*listaBonificaciones)) {
+            while (nodoSiguiente != fin(*listaBonificaciones))
+            {
                 recupera(*listaBonificaciones, nodoSiguiente, &elemento);
-                if (elemento > menorElemento) {
+                if (elemento > menorElemento)
+                {
                     nodoDelMenorElemento = nodoSiguiente;
                     menorElemento = elemento;
                 }
                 nodoSiguiente = siguiente(*listaBonificaciones, nodoSiguiente);
             }
             dineroARecaudar = dineroARecaudar - menorElemento;
-            if (dineroARecaudar < 0) {
-                modifica(listaBonificaciones, nodoDelMenorElemento, -1*dineroARecaudar);
+            if (dineroARecaudar < 0)
+            {
+                modifica(listaBonificaciones, nodoDelMenorElemento, -1 * dineroARecaudar);
                 return;
-            } else {
+            }
+            else
+            {
                 suprime(listaBonificaciones, nodoDelMenorElemento);
             }
         }
-    } else {
+    }
+    else
+    {
         printf("No hay suficientes bonificaciones para recaudar esa cantidad.\n");
     }
 }
 
-booleano comprobarValorARecaudar(TIPOELEMENTOLISTA v) {
-    if (v > 0) {
-        return(TRUE);
-    } else {
-        return(FALSE);
+booleano comprobarValorARecaudar(TIPOELEMENTOLISTA v)
+{
+    if (v > 0)
+    {
+        return (TRUE);
+    }
+    else
+    {
+        return (FALSE);
     }
 }
 
-booleano comprobarNumeroEntradas(int numeroEntradas) {
-    if (numeroEntradas > 0) {
+booleano comprobarNumeroEntradas(int numeroEntradas)
+{
+    if (numeroEntradas > 0)
+    {
         return TRUE;
-    } else {
+    }
+    else
+    {
         return FALSE;
     }
 }
 
-void imprimirPrimeroCola(TCOLA cola) {
+void imprimirPrimeroCola(TCOLA cola)
+{
     TIPOELEMENTOCOLA elemento;
     PrimeroCola(cola, &elemento);
-    printf( (EsColaVacia(cola)) ? ("\n\t\t|===============|\n\t\t|               |\n\t\t|  COLA VACIA   |\n\t\t|               |\n\t\t|===============|\n") : ("\n\t\t|===============|\n\t\t|    EN COLA    |\n\t\t|  %d  ENTRADAS  |\n\t\t|               |\n\t\t|===============|\n"), (int) elemento);
+    printf((EsColaVacia(cola)) ? ("\n\t\t|===============|\n\t\t|               |\n\t\t|  COLA VACIA   |\n\t\t|               |\n\t\t|===============|\n") : ("\n\t\t|===============|\n\t\t|    EN COLA    |\n\t\t|  %d  ENTRADAS  |\n\t\t|               |\n\t\t|===============|\n"), (int)elemento);
 }
 
-void imprimirListaBonificaciones(TLISTA listaBonificaciones) {
+void imprimirListaBonificaciones(TLISTA listaBonificaciones)
+{
     TNODOLISTA nodosiguiente;
     TIPOELEMENTOLISTA elemento;
     char i = 0;
     nodosiguiente = primero(listaBonificaciones);
-        printf("\nLista de bonificaciones:");
-    if (esVacia(listaBonificaciones)) {
+    printf("\nLista de bonificaciones:");
+    if (esVacia(listaBonificaciones))
+    {
         printf("\nNo hay bonificaciones.\n");
-        return;}
-    while (nodosiguiente != fin(listaBonificaciones)) {
-        printf("\n%d. %f", i++, (recupera(listaBonificaciones,nodosiguiente,&elemento), elemento));
+        return;
+    }
+    while (nodosiguiente != fin(listaBonificaciones))
+    {
+        printf("\n%d. %f", i++, (recupera(listaBonificaciones, nodosiguiente, &elemento), elemento));
         nodosiguiente = siguiente(listaBonificaciones, nodosiguiente);
     }
 }
 
-void liberarListaBonificaciones(TLISTA *listaBonificaciones) {
+void liberarListaBonificaciones(TLISTA *listaBonificaciones)
+{
     /*TNODOLISTA nodoSiguiente, nodoActual = primero(*listaBonificaciones);
     while (!esVacia(*listaBonificaciones)) {
         nodoSiguiente = siguiente(*listaBonificaciones, nodoActual);
@@ -97,7 +127,8 @@ void liberarListaBonificaciones(TLISTA *listaBonificaciones) {
     *listaBonificaciones = NULL;
 }
 
-void liberarColaEspera(TCOLA *colaEspera) {
+void liberarColaEspera(TCOLA *colaEspera)
+{
     while (!EsColaVacia(*colaEspera))
     {
         EliminarCola(colaEspera);
@@ -106,7 +137,8 @@ void liberarColaEspera(TCOLA *colaEspera) {
     *colaEspera = NULL;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     char opcion, i;
     unsigned int atendidos = 0, porAtender = 0;
     TIPOELEMENTOCOLA aux;
@@ -118,71 +150,83 @@ int main(int argc, char** argv) {
     ColaVacia(&cola);
     crea(&lista);
 
-    for (i = 1; i < argc; i++) {
-        AnadirCola(&cola, (TIPOELEMENTOCOLA) atoi(argv[i])); //Añadimos los clientes que haga falta a la cola
+    for (i = 1; i < argc; i++)
+    {
+        AnadirCola(&cola, (TIPOELEMENTOCOLA)atoi(argv[i])); //Añadimos los clientes que haga falta a la cola
         porAtender++;
     }
 
-    do {
+    do
+    {
         printf("\n\nIntroduzca su opcion:\n\ta. Poner un cliente a la cola.\n\tb. Atender al primer cliente de la cola.\n\tc. Cobrar bonificaciones.\n\td. Obtener estadisticas.\n\n\ts. Salir del programa.\n\n\tOPCION: ");
-        scanf(" %c",&opcion);
+        scanf(" %c", &opcion);
 
         switch (opcion)
         {
-        case 'a'://Añadir cliente
+        case 'a': //Añadir cliente
             printf("Introduzca el numero de entradas que desea comprar: ");
             scanf("%d", &aux);
-            if (comprobarNumeroEntradas(aux) == TRUE) {
-                AnadirCola(&cola, (TIPOELEMENTOCOLA) aux);
+            if (comprobarNumeroEntradas(aux) == TRUE)
+            {
+                AnadirCola(&cola, (TIPOELEMENTOCOLA)aux);
                 porAtender++;
-            } else {
+            }
+            else
+            {
                 printf("\nNumero de entradas no valido.");
             }
             imprimirPrimeroCola(cola);
             break;
 
-        case 'b'://Despachar cliente
-            if (EsColaVacia(cola)) {
+        case 'b': //Despachar cliente
+            if (EsColaVacia(cola))
+            {
                 printf("La cola esta vacia.");
-            } else {
+            }
+            else
+            {
                 PrimeroCola(cola, &aux);
                 EliminarCola(&cola);
                 porAtender--;
                 atendidos++;
                 imprimirPrimeroCola(cola);
                 //insertamos la bonificacion
-                inserta(&lista, fin(lista), aux*BONIFICACION);
+                inserta(&lista, fin(lista), aux * BONIFICACION);
             }
             imprimirListaBonificaciones(lista);
             break;
 
-        case 'c'://Cobrar bonificaciones
+        case 'c': //Cobrar bonificaciones
             printf("Cuanto desea cobrar? (max: %f): ", totalBonificaciones(lista));
             scanf(" %f", &auxLista);
-            if (comprobarValorARecaudar(auxLista) == TRUE) {
+            if (comprobarValorARecaudar(auxLista) == TRUE)
+            {
                 recaudarBonificaciones(&lista, auxLista, &totalBonificacionesRecaudadas);
                 imprimirListaBonificaciones(lista);
-            } else {
+            }
+            else
+            {
                 printf("La cantidad introducida es incorrecta.\n");
             }
             break;
 
-        case 'd'://Estadísticas
+        case 'd': //Estadísticas
             printf("Atendidos: %d\nEsperando: %d\nRecaudado: %f\nPor recaudar: %f\n", atendidos, porAtender, totalBonificacionesRecaudadas, totalBonificaciones(lista));
             break;
 
         case 's':
             printf("Realmente desea cerrar el programa? Confirme con \"s\": ");
-            scanf(" %c",&opcion);
-            if (opcion == 's'){
+            scanf(" %c", &opcion);
+            if (opcion == 's')
+            {
                 printf("\nSaliendo del programa...\n");
                 liberarListaBonificaciones(&lista);
                 liberarColaEspera(&cola);
             }
             break;
-        
+
         default:
-        printf("\n\nOpcion no valida.\n\n");
+            printf("\n\nOpcion no valida.\n\n");
             break;
         }
     } while (opcion != 's');
