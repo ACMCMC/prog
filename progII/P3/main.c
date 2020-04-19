01455689ba1b0c2fb152ddb26ebdff4119587282
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-04-19 17:44:23 +0200

    Draft de la P3

---
+++ progII/P3/main.c
@@ -0,0 +1,5 @@
+#define EXIT_SUCCESS 0;
+
+int main(int argc, char** argv) {
+    return EXIT_SUCCESS;
+}
---
+++ progII/P3/matrizdinamica.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   matrizdinamica.h
+ * Author: mariajose
+ *
+ * Created on 14 de enero de 2020, 12:43
+ */
+
+#ifndef MATRIZDINAMICA_H
+#define MATRIZDINAMICA_H
+
+/*Tipo de datos de los elementos de la matriz*/
+typedef float TELEMENTO;
+/*tipo opaco, los detalles de implementación están ocultos al usuario*/
+typedef void *matrizD;
+/*Función crear: asigna memoria y devuelve la asignación a la matriz. Recibe m1 por referencia para devolver al programa principal la dirección de memoria reservada por este procedimiento*/
+void crear(matrizD *m1, short nfilas, short ncolumnas);
+/*Función asignar: Llena una posición de la matriz con un valor. Recibe una copia de la dirección de memoria reservada para la matriz m1*/
+void asignar(matrizD *m1, short fila, short columna, TELEMENTO valor);
+
+void liberar(matrizD *m1);
+
+TELEMENTO recuperar(matrizD *m1, short fila, short columna);
+
+short nfilas(matrizD *m1);
+
+short ncolumnas(matrizD *m1);
+
+void prodescalar(matrizD *m1, int escalar);
+
+#endif /* MATRIZDINAMICA_H */
