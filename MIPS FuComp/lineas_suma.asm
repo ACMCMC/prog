f901ec40860827f4516b328efc6e3aa83678f0e1
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-04-26 06:29:46 +0200

    Cambios en el ejercicio de lineas_suma para soportar dígitos > 10

--- MIPS FuComp/lineas_suma.asm
+++ MIPS FuComp/lineas_suma.asm
@@ -49,6 +49,8 @@ subrutina_imprimir_linea: #(a0 = num_max)
 	addi $t1, $zero, '\n' #caracter de nueva linea
 	sb $t1, 0($sp)
 	
+	addi $t3, $zero, 10 #t3 = 10 (para hacer la division)
+	
 	#t0 = i
 	
 	inicializar_for_subrut:
@@ -60,9 +62,34 @@ subrutina_imprimir_linea: #(a0 = num_max)
 		addi $t0, $t0, -1 #i--
 		j condicion_for_subrut
 	cuerpo_for_subrut:
-		addi $t1, $t0, '0' #t1 = i+'0'
+		div $t0, $t3 #t0/10
+		mfhi $t1
+		addi $t1, $t1, '0' #t1 = i+'0'
 		addi $sp, $sp, -1 #sp--
 		sb $t1, 0($sp) #lo guardamos en sp
+		mflo $t4 #t4 = cociente
+		beq $t4, $zero, incremento_for_subrut #se acaba el cuerpo si no hay cociente
+		
+		loop_terminos_b10:
+		add $t5, $zero, $sp #guardamos sp en t5
+		div $t4, $t3 #t4/10
+		mflo $t4 #t4 = cociente
+		mfhi $t1 #t1 = resto
+		addi $t1, $t1, '0' #t1 = i+'0'
+		addi $sp, $sp, -1 #sp--
+		sb $t1, 0($sp) #lo guardamos en sp
+		bne $t4, $zero, loop_terminos_b10 #repetimos esto mientras aun haya cociente
+		#ahora tenemos que darle la vuelta a las cosas del stack
+		#add $t6, $zero, $sp #t6 = sp
+		#dar_vuelta_stack:
+		#	lb $t8, 0($t5) #les damos la vuelta a los elementos
+		#	lb $t9, 0($t6)
+		#	sb $t9, 0($t5)
+		#	sb $t8, 0($t6)
+		#	addi $t6, $t6, 1 #acercamos ambos elementos
+		#	addi $t5, $t5, -1
+		#	sle $t7, $t5, $t6 #t7 = (t5 <= t6)
+		#	beq $t7, $zero, dar_vuelta_stack
 		j incremento_for_subrut
 	fin_for_subrut:
 	add $a0, $zero, $sp #a0 = sp
